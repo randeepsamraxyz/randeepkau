e39bb0c6dbb23b0ca7ee7f3312738e73d91ff2a6
Randeep Kaur <731777@student.tor.loc>
2019-03-28 16:38:24 -0400

    4:32PM

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Gagan rules the IT universe"); Download();
+        }
+
+        static void Download()
+        {
+            Network.Download();
+        }
+
+    }
+    class Network
+    {
+        static public Task Download()
+        {
+            return Task.Run(() => Thread.Sleep(3000));
+        }
+    }
+}
+
